decfd6dadeafd4cdb312fb124facd219ebe4509b
Steve <Steve.Kuhn306@gmail.com>
2019-07-15 16:15:36 -0400

    Bug fix 1

--- mtp/src/App.js
+++ mtp/src/App.js
@@ -8,7 +8,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename='/'>
         <div>
           <Header />
           <Main />
