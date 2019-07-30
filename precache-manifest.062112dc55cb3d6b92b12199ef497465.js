3f08881728b73e882499fd6b1cee52a9592f1194
Steve <Steve.Kuhn306@gmail.com>
2019-07-30 09:11:06 -0400

    Updates

---
+++ precache-manifest.062112dc55cb3d6b92b12199ef497465.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63a7c5b0c4cfd4461f65ae5c61ecfa5",
+    "url": "/WDD5/index.html"
+  },
+  {
+    "revision": "980015a063bfad4242ef",
+    "url": "/WDD5/static/css/main.5e48a23f.chunk.css"
+  },
+  {
+    "revision": "08c45b8fd68537c0b69b",
+    "url": "/WDD5/static/js/2.e062634d.chunk.js"
+  },
+  {
+    "revision": "980015a063bfad4242ef",
+    "url": "/WDD5/static/js/main.9ae5dde5.chunk.js"
+  },
+  {
+    "revision": "aa287226f9e67b6998c0",
+    "url": "/WDD5/static/js/runtime~main.3be0ea27.js"
+  },
+  {
+    "revision": "d274969d4761a1cd5d215701465d0d93",
+    "url": "/WDD5/static/media/insightArt.d274969d.jpg"
+  },
+  {
+    "revision": "f13c2ef7b115249d944bb9f56d5df324",
+    "url": "/WDD5/static/media/mars.f13c2ef7.jpg"
+  },
+  {
+    "revision": "759988b30f084f71ba39e916b5df4e0f",
+    "url": "/WDD5/static/media/mtpLogo.759988b3.png"
+  },
+  {
+    "revision": "e67baa43d167a9dbc761d9db96ab240b",
+    "url": "/WDD5/static/media/spaceXFH.e67baa43.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WDD5/precache-manifest.3b8032db8808e0c5db066930e7e8f155.js"
+  "/WDD5/precache-manifest.062112dc55cb3d6b92b12199ef497465.js"
 );
 
 self.addEventListener('message', (event) => {
